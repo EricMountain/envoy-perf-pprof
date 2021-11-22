8d0d0b8955acb84955c52b1e7dbc9fc3c7b7649e
Eric Mountain <eric.mountain@datadoghq.com>
2021-11-22 13:28:56 +0100

    Allow multi-arch build

--- envoy-perf-pprof.sh
+++ envoy-perf-pprof.sh
@@ -2,10 +2,11 @@
 
 set -euo pipefail
 
-ENVOY_VERSION=$1
-FILE="$(cd "$(dirname "$2")" && pwd -P)/$(basename "$2")"
-[ -z "$ENVOY_VERSION" ] || [ -z "$FILE" ] && echo "usage: ./envoy-perf-pprof.sh <envoy-version> <perf-record-file>" && exit 1
+ARCH=$1
+ENVOY_VERSION=$2
+FILE="$(cd "$(dirname "$3")" && pwd -P)/$(basename "$3")"
+[ -z "$ARCH" ] || [ -z "$ENVOY_VERSION" ] || [ -z "$FILE" ] && echo "usage: ./envoy-perf-pprof.sh <cpu_arch> <envoy-version> <perf-record-file>" && exit 1
 
-docker build -t envoy-perf-pprof --build-arg ENVOY_VERSION=$ENVOY_VERSION .
-docker run -p 8888:8888 -v $FILE:/root/envoy.perf envoy-perf-pprof /root/envoy.perf
+docker build --platform linux/${ARCH} -t envoy-perf-pprof-${ARCH}-${ENVOY_VERSION} --build-arg ENVOY_VERSION=$ENVOY_VERSION .
+docker run --platform linux/${ARCH} --rm -p 8888:8888 -v $FILE:/root/envoy.perf envoy-perf-pprof-${ARCH}-${ENVOY_VERSION} /root/envoy.perf
 
